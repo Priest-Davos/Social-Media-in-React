8e1395bf8cfc7d9cd3b6ada772bc30f19b6e6698
priest davos <rajjo1920london@gmail.com>
2024-03-12 07:32:33 +0530

    Modified form in CreatePost component according to the data we need from the user as input like(userId,PostTitle,PostContent,NoOfReactions,Hashtags) will update some inputField in future as userid we will get like from their login detail , reactions

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -1,20 +1,34 @@
 const CreatePost=()=>{
   return(
-    <form>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"></input>
-    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+    <form className="create-post">
+    <div className="mb-3">
+    <label for="userId" className="form-label">User ID</label>
+    <input type="text" className="form-control" id="userId" placeholder="Enter your User ID here" ></input>
+   
   </div>
-  <div class="mb-3">
-    <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1"></input>
+  <div className="mb-3">
+    <label for="title" className="form-label">Post Title</label>
+    <input type="text" className="form-control" id="title" placeholder="How are you feeling today..." ></input>
+   
   </div>
-  <div class="mb-3 form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1"></input>
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  <div className="mb-3">
+    <label for="body" className="form-label">Post Content</label>
+    <textarea type="text"  rows="3" className="form-control" id="body" placeholder="Enter the post message here" ></textarea>
+   
   </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
+ 
+  <div className="mb-3">
+    <label for="reactions" className="form-label">Number of reactions</label>
+    <input type="text" className="form-control" id="reactions" placeholder="How many people reacted to this post" ></input>
+   
+  </div>
+  <div className="mb-3">
+  <label for="tags" className="form-label">Enter your hashtags here</label>
+  <input type="text" className="form-control" id="tags" placeholder="Please separate multiple tags by space" ></input>
+ 
+</div>
+ 
+  <button type="submit" className="btn btn-primary">Post</button>
 </form>
   )
 }
