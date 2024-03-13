87dd7417c65ee06197a6e0f9a0d65049efca38b9
priest davos <rajjo1920london@gmail.com>
2024-03-13 20:37:20 +0530

    copied hero from bootstrap & pasted in welcomeMsg

--- src/components/WelcumMsg.jsx
+++ src/components/WelcumMsg.jsx
@@ -1,5 +1,19 @@
 const WelcumMsg=()=>
 {
-  return(<center><h1 className="Welcum-msg">Empty post List</h1></center>)
+  //return(<center><h1 className="Welcum-msg">Empty post List</h1></center>)
+return(
+  <div class="bg-dark text-secondary px-4 py-5 text-center">
+  <div class="py-5">
+    <h1 class="display-5 fw-bold text-white">😎😎😎 Welcome 😎😎😎 </h1>
+    <div class="col-lg-6 mx-auto">
+      <p class="fs-5 mb-4">Currently no post here</p>
+      <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+        <button type="button" class="btn btn-outline-info btn-lg px-4 me-sm-3 fw-bold">Custom button</button>
+        
+      </div>
+    </div>
+  </div>
+</div>
+)
 }
 export default WelcumMsg
